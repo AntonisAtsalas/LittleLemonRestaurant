ae55b5e414238023cc9fd0c0118d1a392134ddae
unknown <antonis.atsalas@gmail.com>
2023-10-04 16:03:45 +0300

    dd

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('books/', views.books)
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-@api_view()
-def books(request):
-    return Response('list of the books', status=status.HTTP_200_OK)
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
